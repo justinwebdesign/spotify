4bbd0d4741904072fb855c98d3d908a1bd5f9691
justinwebdesign <Justincreamer222@gmail.com>
2025-05-15 11:47:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -234,4 +234,3 @@ function showExitPopup() {
     window.location.href = "https://fmhy.net/audiopiracyguide"; // Redirects to the specified URL
   }
 }
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
